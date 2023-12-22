d8de625a3d11f4cbb8cee84b4e58028ccc354ac0
Tabassum <sadia.tabassum@accenture.com>
2023-12-22 23:37:08 +0530

    Add to remote GitHub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoList';
+
+  constructor(){
+
+  // setTimeout(() => {
+  //   this.title = "Changed title";
+
+  // }, 2000);
+  
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './myComponent/todos/todos.component';
+import { TodoItemComponent } from './myComponent/todo-item/todo-item.component';
+import { AddTodoComponent } from './myComponent/add-todo/add-todo.component';
+import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './myComponent/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    AddTodoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
